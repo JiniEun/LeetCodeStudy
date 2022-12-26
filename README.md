23d7f10eeda2f0092d325c4d6caa8cd49ed3a99c
[상품상세개발팀]김은진 <eunjin.kim01@lotte.net>
2022-12-26 10:46:48 +0900

    merge: main branch

---
+++ README.md
@@ -0,0 +1,8 @@
+# LeetCodeStudy
+LeetCodeStudy
+
+<br/>
+
+### [LeetCode 75](https://leetcode.com/study-plan/leetcode-75/?progress=x7s5ykj6)
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
-import problem1480.Solution1480;
-
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-//        Solution1480.run();
+
     }
 }
