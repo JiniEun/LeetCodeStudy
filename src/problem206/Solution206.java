07cdada2d1b3951cab93b6d808f0e2f1da62bb8c
JiniEun <julietjin94@gmail.com>
2022-12-28 23:47:05 +0900

    feat: solve problem 206

---
+++ src/problem206/Solution206.java
@@ -0,0 +1,33 @@
+package problem206;
+
+import java.util.HashMap;
+
+public class Solution206 {
+    public static ListNode reverseList(ListNode head) {
+        ListNode node = null;
+        while(head != null) {
+            ListNode temp = head;
+            head = head.next;
+            temp.next = node;
+            node = temp;
+        }
+        return node;
+    }
+
+    public static void printAnswer() {
+        // ListNode list2 = null;
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        ListNode result = reverseList(head);
+        String resultListNode = "";
+        while (result != null) {
+            resultListNode += result.val + " ";
+            result = result.next;
+        }
+        resultListNode = resultListNode.substring(0, resultListNode.length() - 1);
+        System.out.println(resultListNode);
+    }
+
+    public static void run() {
+        printAnswer();
+    }
+}
--- src/problem21/Solution21.java
+++ src/problem21/Solution21.java
@@ -1,62 +1,46 @@
 package problem21;
 
 public class Solution21 {
-	static class ListNode {
-		int val;
-		ListNode next;
 
-		ListNode() {
-		}
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode result = new ListNode();
+        ListNode temp = result;
 
-		ListNode(int val) {
-			this.val = val;
-		}
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                temp.next = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+            }
+            temp = temp.next;
+        }
 
-		ListNode(int val, ListNode next) {
-			this.val = val;
-			this.next = next;
-		}
-	}
+        if (list1 == null) {
+            temp.next = list2;
+        } else {
+            temp.next = list1;
+        }
 
-	public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-		ListNode result = new ListNode();
-		ListNode temp = result;
+        return result.next;
+    }
 
-		while (list1 != null && list2 != null) {
-			if (list1.val < list2.val) {
-				temp.next = list1;
-				list1 = list1.next;
-			} else {
-				temp.next = list2;
-				list2 = list2.next;
-			}
-			temp = temp.next;
-		}
+    public static void printAnswer() {
+        // ListNode list2 = null;
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        ListNode result = mergeTwoLists(list1, list2);
+        String resultListNode = "";
+        while (result != null) {
+            resultListNode += result.val + " ";
+            result = result.next;
+        }
+        resultListNode = resultListNode.substring(0, resultListNode.length() - 1);
+        System.out.println(resultListNode);
+    }
 
-		if (list1 == null) {
-			temp.next = list2;
-		} else {
-			temp.next = list1;
-		}
-
-		return result.next;
-	}
-
-	public static void printAnswer() {
-		// ListNode list2 = null;
-		ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
-		ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
-		ListNode result = mergeTwoLists(list1, list2);
-		String resultListNode = "";
-		while (result != null) {
-			resultListNode += result.val + " ";
-			result = result.next;
-		}
-		resultListNode = resultListNode.substring(0, resultListNode.length() - 1);
-		System.out.println(resultListNode);
-	}
-
-	public static void run() {
-		printAnswer();
-	}
+    public static void run() {
+        printAnswer();
+    }
 }
