0835106ad3d9336df89c8e3c60ab1a36eda6b345
[상품상세개발팀]김은진 <eunjin.kim01@lotte.net>
2022-12-29 18:23:02 +0900

    feat: solve problem 876

---
+++ src/problem876/Solution876.java
@@ -0,0 +1,42 @@
+package problem876;
+
+public class Solution876 {
+
+	public static ListNode middleNode(ListNode head) {
+
+		// 현재 ListNode의 길이를 구하고, 가운데 노드를 반환한다
+		ListNode currentNode = head;
+		int len = 0;
+
+		while (currentNode != null) {
+			len++;
+			currentNode = currentNode.next;
+		}
+
+		currentNode = head;
+
+		int mid = len / 2;
+
+		for (int i = 0; i < mid; i++) {
+			currentNode = currentNode.next;
+		}
+		return currentNode;
+	}
+
+	public static void printAnswer() {
+		// ListNode list2 = null;
+		ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+		ListNode result = middleNode(head);
+		String resultListNode = "";
+		while (result != null) {
+			resultListNode += result.val + " ";
+			result = result.next;
+		}
+		resultListNode = resultListNode.substring(0, resultListNode.length() - 1);
+		System.out.println(resultListNode);
+	}
+
+	public static void run() {
+		printAnswer();
+	}
+}
