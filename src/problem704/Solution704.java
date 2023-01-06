27d7f3b83ef13e716f22833f8281c365fd9437b1
JiniEun <julietjin94@gmail.com>
2023-01-06 18:20:04 +0900

    feat: solve problem 704

---
+++ src/problem704/Solution704.java
@@ -0,0 +1,14 @@
+package problem704;
+
+public class Solution704 {
+    public int search(int[] nums, int target) {
+        int index = -1;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == target){
+                index = i;
+                return index;
+            }
+        }
+        return index;
+    }
+}
